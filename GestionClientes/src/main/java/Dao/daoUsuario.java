7fbb7fc8a6502359e2e0b4678d3264cfc20fd8a4
Valentin <lopezvalentinoscar@gmail.com>
2025-11-22 16:44:21 -0300

    daoUsuario + corrección en base de datos.

--- GestionClientes/src/main/java/Dao/daoUsuario.java
+++ GestionClientes/src/main/java/Dao/daoUsuario.java
@@ -1,7 +1,40 @@
 package Dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Entidades.usuario;
 
 public class daoUsuario {
 	
+	private String host= "jdbc:mysql://localhost:3306/sistema_clientes";
+	private String user= "root";
+	private String pass= "root";
 	
-
+	public usuario login(String mail, String contrasena)
+	{
+		String query = "SELECT * FROM usuarios WHERE correo_electronico = ? AND contrasena = ?";
+		usuario u = new usuario();
+		
+		try(Connection cn = DriverManager.getConnection(host, user, pass);
+			PreparedStatement ps = cn.prepareStatement(query);){
+			
+			ps.setString(1, mail);
+			ps.setString(2, contrasena);
+		
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				u.setId(rs.getInt("id_usuario"));
+				u.setNombre(rs.getString("usuario"));
+				u.setMail(rs.getString("correo_electronico"));
+				u.setContrasena(rs.getString("contrasena"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return u;
+	}
 }
--- GestionClientes/src/main/java/Entidades/usuario.java
+++ GestionClientes/src/main/java/Entidades/usuario.java
@@ -4,6 +4,7 @@ public class usuario {
 	
 	private int id;
 	private String nombre;
+	private String mail;
 	private String contrasena;
 	
 	public usuario(){}
@@ -25,6 +26,12 @@ public class usuario {
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
 	}
 	public String getContrasena() {
 		return contrasena;
